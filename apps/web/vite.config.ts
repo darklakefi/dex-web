30a0710120347b9ed0d3458b7acb2f161ad6005e
lewisflude <lewis@lewisflude.com>
2025-05-14 15:54:39 +0100

    chore: :sparkles: Add Vite and Vitest configuration files, update package dependencies, and refine TypeScript settings

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,26 @@
+/// <refenxCopyAssetsPlugin'vitest'@nx/vite/plugins/nx-copy-assets.plugin';
+/// <reference types='vitest' />
+import react from '@vitejs/plugin-react';
+/// <reference types='vitest' />
+import { defineConfig
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/web',
+  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'jsdom',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/web',
+      provider: 'v8' as const,
+    },
+  },
+}));
