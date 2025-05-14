25a4a610023c23bd33932f72b410648384cc99c1
lewisflude <lewis@lewisflude.com>
2025-05-14 15:55:53 +0100

    chore: :sparkles: Update Vite configuration to use happy-dom for testing environment and refine asset copying with nxCopyAssetsPlugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,21 +1,16 @@
-/// <refenxCopyAssetsPlugin'vitest'@nx/vite/plugins/nx-copy-assets.plugin';
-/// <reference types='vitest' />
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
-/// <reference types='vitest' />
-import { defineConfig
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/web',
   plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
   test: {
     watch: false,
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
     coverage: {
