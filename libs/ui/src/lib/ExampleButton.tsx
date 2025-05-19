fb65f60caa444318917f3e01a3792eec7cfa0581
Felix <hdcompany123@gmail.com>
2025-05-19 12:08:33 +0700

    chore: set indent style for biome

--- libs/ui/src/lib/ExampleButton.tsx
+++ libs/ui/src/lib/ExampleButton.tsx
@@ -1,28 +1,28 @@
 import { type VariantProps, cva } from "class-variance-authority";
 
 const buttonVariants = cva("rounded-md bg-blue-500 p-2 text-white", {
-	variants: {
-		variant: {
-			primary: "bg-blue-500",
-			secondary: "bg-red-500",
-		},
-	},
+  variants: {
+    variant: {
+      primary: "bg-blue-500",
+      secondary: "bg-red-500",
+    },
+  },
 });
 
 type ExampleButtonPropsVariantProps = VariantProps<typeof buttonVariants>;
 interface ExampleButtonProps extends ExampleButtonPropsVariantProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function ExampleButton({
-	children,
-	variant = "primary",
+  children,
+  variant = "primary",
 }: ExampleButtonProps) {
-	return (
-		<button className={buttonVariants({ variant })} type="button">
-			{children}
-		</button>
-	);
+  return (
+    <button className={buttonVariants({ variant })} type="button">
+      {children}
+    </button>
+  );
 }
 
 export default ExampleButton;
