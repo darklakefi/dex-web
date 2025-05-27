0964f7de2d039632a0477aa25d9e7a03b0bb4400
lewisflude <lewis@lewisflude.com>
2025-05-27 20:17:30 +0100

    style(ui): refine TextInput component styles for improved usability and aesthetics

--- libs/ui/src/lib/TextInput.tsx
+++ libs/ui/src/lib/TextInput.tsx
@@ -2,7 +2,7 @@ import { type VariantProps, cva } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
 const textInputVariants = cva(
-  "peer flex p-2 placeholder-transparent focus:outline-none focus:ring-2 focus:ring-blue-500",
+  "peer flex py-2 pr-2 pl-6 placeholder-transparent caret-green-300 focus:outline-none focus:ring-2 focus:ring-blue-200",
   {
     variants: {},
     defaultVariants: {},
@@ -29,7 +29,8 @@ export function TextInput({
   ...props
 }: TextInputProps) {
   return (
-    <label className="relative inline-flex border border-green-300 bg-green-600 font-sans text-green-300">
+    <label className="relative inline-flex items-center border border-green-300 bg-green-600 font-sans text-green-300 text-lg/4xl">
+      <span className="absolute top-0 left-0 py-2 pl-2">{"> "}</span>
       <input
         onChange={onChange}
         value={value}
@@ -41,7 +42,7 @@ export function TextInput({
         )}
         {...props}
       />
-      <span className="act invisible absolute top-2 left-2 text-green-300 peer-placeholder-shown:visible peer-focus:text-green-400 peer-active:text-green-400">
+      <span className="invisible absolute top-2 left-6 text-green-300 peer-placeholder-shown:visible peer-focus:text-green-400 peer-active:text-green-400">
         {label}
       </span>
     </label>
