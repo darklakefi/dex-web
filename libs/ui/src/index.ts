bfa238f114c961e7e92b9017ed4cd9b19f8b876f
lewisflude <lewis@lewisflude.com>
2025-06-02 15:51:40 +0100

    feat(ui): export Header and Icon components from index file

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,4 +1,6 @@
-export * from "./lib/Box/Box";
-export * from "./lib/Button/Button";
-export * from "./lib/Text/Text";
-export * from "./lib/TextInput/TextInput";
+export { Box } from "./lib/Box/Box";
+export { Button } from "./lib/Button/Button";
+export { Header } from "./lib/Header/Header";
+export { Icon } from "./lib/Icon/Icon";
+export { Text } from "./lib/Text/Text";
+export { TextInput } from "./lib/TextInput/TextInput";
