83ebac0027a3b1043c9699c50a96776eb5d1cc5c
lewisflude <lewis@lewisflude.com>
2025-06-06 15:06:26 +0100

    fix: update root.spec.ts to check for 'Under construction' in paragraph instead of h1

--- apps/web-e2e/src/root.spec.ts
+++ apps/web-e2e/src/root.spec.ts
@@ -4,5 +4,5 @@ test("has title", async ({ page }) => {
   await page.goto("/");
 
   // Expect h1 to contain a substring.
-  expect(await page.locator("h1").innerText()).toContain("Under construction");
+  expect(await page.locator("p").innerText()).toContain("Under construction");
 });
