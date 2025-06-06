4de9c10b19f5cde267caa7c1f4657a2487d221fc
lewisflude <lewis@lewisflude.com>
2025-06-06 16:42:00 +0100

    feat: generate tRPC config

---
+++ libs/api/vite.config.ts
@@ -0,0 +1,61 @@
+/// <reference types="vitest" />
+import { join, resolve } from "node:path";
+import { defineConfig, mergeConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig(() => {
+  const baseConfig = {
+    root: __dirname,
+    cacheDir: "../../node_modules/.vite/libs/utils",
+    test: {
+      name: "utils",
+      environment: "happy-dom",
+      include: [
+        "src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        "tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+      ],
+      coverage: {
+        provider: "v8" as const,
+        reportsDirectory: "../../coverage/libs/utils",
+      },
+      watch: false,
+      reporters: ["default"],
+      testTimeout: 30000,
+      hookTimeout: 30000,
+      teardownTimeout: 10000,
+      pool: "forks" as const,
+      poolOptions: {
+        forks: {
+          singleFork: true,
+        },
+      },
+      globals: true,
+    },
+    build: {
+      outDir: "./dist",
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "@dex-web/utils",
+        fileName: "index",
+        formats: ["es" as const],
+      },
+      rollupOptions: {
+        external: [],
+      },
+    },
+  };
+
+  return mergeConfig(baseConfig, {
+    plugins: [
+      dts({
+        entryRoot: "src",
+        tsconfigPath: join(__dirname, "tsconfig.lib.json"),
+      }),
+    ],
+  });
+});
