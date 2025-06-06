2257148f82faa0d00c2979aed57516ce0319711b
Felix <hdcompany123@gmail.com>
2025-06-07 03:06:40 +0700

    fix: e2e test

--- apps/web-e2e/src/root.spec.ts
+++ apps/web-e2e/src/root.spec.ts
@@ -3,8 +3,8 @@ import { expect, test } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
-  // Expect p to contain a substring.
-  expect(await page.locator("p").innerText()).toContain(
+  // Expect the first paragraph to contain a substring.
+  expect(await page.locator("p").first().innerText()).toContain(
     "UNDER CONSTRUCTION 🚧",
   );
 });
