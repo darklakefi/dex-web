cd27eeb29fbf365fc4c2a28feca69f028fd4cf11
lewisflude <lewis@lewisflude.com>
2025-06-06 15:08:00 +0100

    fix: update root.spec.ts to check for 'UNDER CONSTRUCTION 🚧' in paragraph

--- apps/web-e2e/src/root.spec.ts
+++ apps/web-e2e/src/root.spec.ts
@@ -3,6 +3,8 @@ import { expect, test } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator("p").innerText()).toContain("Under construction");
+  // Expect p to contain a substring.
+  expect(await page.locator("p").innerText()).toContain(
+    "UNDER CONSTRUCTION 🚧",
+  );
 });
