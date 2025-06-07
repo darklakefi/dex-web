c0620424e5004fe2a1ca5fb3dab604f87d3268d2
lewisflude <lewis@lewisflude.com>
2025-06-07 13:33:26 +0100

    chore: update dependencies in package.json and pnpm-lock.yaml

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
 import { join } from "node:path";
 /// <reference types='vitest' />
 import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 import svgr from "vite-plugin-svgr";
 import { defineConfig, mergeConfig } from "vitest/config";
