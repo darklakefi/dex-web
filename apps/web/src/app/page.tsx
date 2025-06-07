5256c85ff3d7d40f5a115a183c4e546a4e341b82
lewisflude <lewis@lewisflude.com>
2025-06-07 14:58:36 +0100

    refactor: clean up JSX structure in the Index component for improved readability

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,14 +5,12 @@ export default async function Index() {
   const user = await client.user.list({});
   console.log(user);
   return (
-    <div>
-      <div className="wrapper">
-        <div className="container">
-          <div id="welcome" className="flex items-center justify-center">
-            <Text.Body1 className="animate-bounce">
-              Under construction 🚧
-            </Text.Body1>
-          </div>
+    <div className="wrapper">
+      <div className="container">
+        <div id="welcome" className="flex items-center justify-center">
+          <Text.Body1 className="animate-bounce">
+            Under construction 🚧
+          </Text.Body1>
         </div>
       </div>
     </div>
