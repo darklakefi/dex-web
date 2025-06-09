2645f86651269bf3c0c04d118ee93d1c791517d4
lewisflude <lewis@lewisflude.com>
2025-06-09 16:37:43 +0100

    feat: add CORS support to RPC handler and enable OPTIONS method

--- apps/web/src/app/rpc/[[...rest]]/route.ts
+++ apps/web/src/app/rpc/[[...rest]]/route.ts
@@ -1,7 +1,15 @@
 import { router } from "@dex-web/orpc";
 import { RPCHandler } from "@orpc/server/fetch";
+import { CORSPlugin } from "@orpc/server/plugins";
 
-const handler = new RPCHandler(router);
+const handler = new RPCHandler(router, {
+  plugins: [
+    new CORSPlugin({
+      origin: (origin) => origin,
+      allowMethods: ["GET", "HEAD", "PUT", "POST", "DELETE", "PATCH"],
+    }),
+  ],
+});
 
 async function handleRequest(request: Request) {
   const { response } = await handler.handle(request, {
