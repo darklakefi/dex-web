c05d1b889fd5c953989cf452d621df71a397ecdc
lewisflude <lewis@lewisflude.com>
2025-06-13 14:45:49 +0100

    refactor: update SwapForm component structure and add label for selling

--- apps/web/src/app/components/SwapForm.tsx
+++ apps/web/src/app/components/SwapForm.tsx
@@ -1,10 +1,16 @@
-import { Box } from "@dex-web/ui";
+import { Box, Text } from "@dex-web/ui";
 
 export function SwapForm() {
   return (
-    <Box className="w-full max-w-xl flex-col" padding="lg">
-      <Box background="highlight" />
-      <Box background="highlight" />
-    </Box>
+    <section className="flex w-full max-w-xl">
+      <Box className="flex-col" padding="lg">
+        <Box background="highlight">
+          <Text.Body2 as="label" className="text-green-300 uppercase">
+            Selling
+          </Text.Body2>
+        </Box>
+        <Box background="highlight" />
+      </Box>
+    </section>
   );
 }
