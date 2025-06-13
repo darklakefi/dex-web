9857701b165f7b1d150b039060b6ad5a87638168
lewisflude <lewis@lewisflude.com>
2025-06-13 16:09:16 +0100

    feat: add NumericInput component and integrate into SwapForm; update styles and constants

---
+++ apps/web/src/app/constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_BUY_TOKEN = "SOL";
+export const DEFAULT_SELL_TOKEN = "JUP";
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,27 @@
 import { Text } from "@dex-web/ui";
 import { SwapForm } from "./components/SwapForm";
+import { DEFAULT_BUY_TOKEN, DEFAULT_SELL_TOKEN } from "./constants";
 
-export default function IndexPage() {
+export default function SwapPage({
+  searchParams,
+}: {
+  searchParams: { buyToken: string; sellToken: string };
+}) {
+  const { buyToken = DEFAULT_BUY_TOKEN, sellToken = DEFAULT_SELL_TOKEN } =
+    searchParams;
+
+  const buyTokenBalance = 100;
+  const sellTokenBalance = 100;
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Text.Body1 className="animate-bounce">Under construction 🚧</Text.Body1>
-      <SwapForm />
+      <SwapForm
+        buyToken={buyToken}
+        buyTokenBalance={buyTokenBalance}
+        sellToken={sellToken}
+        sellTokenBalance={sellTokenBalance}
+      />
     </div>
   );
 }
