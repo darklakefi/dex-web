b176023ddf08e7a0e741b38c56b976a30323d2c0
lewisflude <lewis@lewisflude.com>
2025-06-18 13:39:51 +0100

    feat: add useDebouncedValue hook and update tsconfig for React JSX support

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,3 +1,5 @@
+export { debounce } from "./common/debounce";
 export { getBaseUrl } from "./common/getBaseUrl";
 export { parseJWT } from "./common/parseJwt";
 export { truncate } from "./common/truncate";
+export { useDebouncedValue } from "./hooks/useDebouncedValue";
