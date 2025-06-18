2162d7153d1f193390c5c14b429beb46417bc596
lewisflude <lewis@lewisflude.com>
2025-06-18 22:24:28 +0100

    chore: enable coverage reporting in nx.json

--- apps/web-e2e/src/root.spec.ts
+++ apps/web-e2e/src/root.spec.ts
@@ -3,7 +3,6 @@ import { expect, test } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
-  // Expect the first paragraph to contain a substring.
   expect(await page.locator("label").first().innerText()).toContain("SELLING");
 
   await expect(page).toHaveScreenshot();
