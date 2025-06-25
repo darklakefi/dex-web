ffc2927d309061ec3e936041a2d65c68d4a55c54
lewisflude <lewis@lewisflude.com>
2025-06-25 15:51:26 +0100

    refactor: remove unused mock setup from web vitest

--- apps/web/vitest.setup.ts
+++ apps/web/vitest.setup.ts
@@ -1,8 +1,6 @@
-import { setupMockOrpcHandlers } from "@dex-web/orpc/mocks";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
 afterEach(() => {
-  setupMockOrpcHandlers();
   cleanup();
 });
