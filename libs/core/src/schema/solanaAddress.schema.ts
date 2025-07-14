bd72ecec8acbcc834b4207b7944b18187ec283eb
Felix <hdcompany123@gmail.com>
2025-07-14 19:37:21 +0700

    chore: remove redundant comment from Solana address schema

--- libs/core/src/schema/solanaAddress.schema.ts
+++ libs/core/src/schema/solanaAddress.schema.ts
@@ -4,7 +4,6 @@ import { z } from "zod";
 const SolanaAddressSchema = z.string().refine(
   (value) => {
     try {
-      // Solana addresses are base58 encoded and typically 32-44 characters long
       if (!/^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(value)) {
         return false;
       }
