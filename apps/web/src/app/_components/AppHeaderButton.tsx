f5672d67f0224818c7ce17130a8af77180d5b8d3
lewisflude <lewis@lewisflude.com>
2025-08-01 13:00:35 +0100

    fix: select wallet logic

--- apps/web/src/app/_components/AppHeaderButton.tsx
+++ apps/web/src/app/_components/AppHeaderButton.tsx
@@ -1,30 +1,15 @@
 "use client";
 
 import { useWallet } from "@solana/wallet-adapter-react";
-import { getFirstConnectedWalletAdapter } from "../_utils/getFirstConnectedWalletAdapter";
-import { getFirstConnectedWalletAddress } from "../_utils/getFirstConnectedWalletAddress";
 import { ConnectedWalletButton } from "./ConnectedWalletButton";
 import { ConnectWalletButton } from "./ConnectWalletButton";
 
 export function AppHeaderButton() {
-  const { wallets } = useWallet();
-  const firstConnectedWalletAdapter = getFirstConnectedWalletAdapter(wallets);
+  const { wallets, wallet } = useWallet();
 
-  const firstConnectedWalletAddress = firstConnectedWalletAdapter
-    ? getFirstConnectedWalletAddress(firstConnectedWalletAdapter)
-    : null;
-
-  if (!firstConnectedWalletAddress || !firstConnectedWalletAdapter) {
+  if (!wallet) {
     return <ConnectWalletButton wallets={wallets} />;
   }
 
-  return (
-    <ConnectedWalletButton
-      walletAdapter={{
-        address: firstConnectedWalletAddress,
-        icon: firstConnectedWalletAdapter.icon,
-        name: firstConnectedWalletAdapter.name,
-      }}
-    />
-  );
+  return <ConnectedWalletButton />;
 }
