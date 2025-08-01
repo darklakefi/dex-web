e764eb00746d782da739ced2df9e282cc69bd47d
jsonDoge <16711523+jsonDoge@users.noreply.github.com>
2025-08-01 18:15:22 +0300

    fix: converting

--- libs/orpc/src/schemas/swaps/getSwapRate.schema.ts
+++ libs/orpc/src/schemas/swaps/getSwapRate.schema.ts
@@ -2,19 +2,19 @@ import { z } from "zod/v4";
 import { tokenSchema } from "../tokens/token.schema";
 
 export const getSwapRateInputSchema = z.object({
-  amountIn: z.number().positive(),
+  amountIn: z.float64().positive(),
   isXtoY: z.boolean(),
   tokenXMint: z.string(),
   tokenYMint: z.string(),
 });
 
 export const getSwapRateOutputSchema = z.object({
-  amountIn: z.number().positive(),
+  amountIn: z.float64().positive(),
   amountInRaw: z.number().positive(),
-  amountOut: z.number().positive(),
+  amountOut: z.float64().positive(),
   amountOutRaw: z.number().positive(),
   estimatedFee: z.number().positive(),
-  rateXtoY: z.number().positive(),
+  rateXtoY: z.float64().positive(),
   tokenX: tokenSchema,
   tokenY: tokenSchema,
 });
