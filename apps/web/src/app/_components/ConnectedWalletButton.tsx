91307a413cc4b7b258d780f9cea74b0ff9a3411e
Felix <hdcompany123@gmail.com>
2025-08-01 15:54:14 +0700

    feat: add disconnect wallet button

--- apps/web/src/app/_components/ConnectedWalletButton.tsx
+++ apps/web/src/app/_components/ConnectedWalletButton.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { Button } from "@dex-web/ui";
+import { Box, Button } from "@dex-web/ui";
 import { truncate } from "@dex-web/utils";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { useWallet } from "@solana/wallet-adapter-react";
 import Image from "next/image";
 
 interface ConnectedWalletButtonProps {
@@ -13,15 +15,47 @@ interface ConnectedWalletButtonProps {
 export function ConnectedWalletButton({
   walletAdapter,
 }: ConnectedWalletButtonProps) {
+  const { disconnect } = useWallet();
+
   return (
-    <Button as="div" className="normal-case" variant="secondary">
-      <Image
-        alt={walletAdapter.name}
-        height={18}
-        src={walletAdapter.icon}
-        width={18}
-      />
-      {truncate(walletAdapter.address)}
-    </Button>
+    <Popover className="">
+      {({ open }) => (
+        <>
+          <PopoverButton
+            as="div"
+            className={open ? "opacity-70" : "opacity-100"}
+          >
+            <Button
+              as="div"
+              className="cursor-pointer normal-case"
+              variant="secondary"
+            >
+              <Image
+                alt={walletAdapter.name}
+                height={18}
+                src={walletAdapter.icon}
+                width={18}
+              />
+              {truncate(walletAdapter.address)}
+            </Button>
+          </PopoverButton>
+          <PopoverPanel anchor="bottom" className="z-30 mt-1">
+            {({ close }) => (
+              <Box className="bg-green-600" padding="sm" shadow="sm">
+                <Button
+                  className="cursor-pointer"
+                  onClick={() => {
+                    close();
+                    disconnect();
+                  }}
+                  text="disconnect"
+                  variant="primary"
+                />
+              </Box>
+            )}
+          </PopoverPanel>
+        </>
+      )}
+    </Popover>
   );
 }
