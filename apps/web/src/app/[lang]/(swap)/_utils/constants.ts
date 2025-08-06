464208aab8fc02a9dca70bf1877313b346359f56
Felix <hdcompany123@gmail.com>
2025-08-06 20:24:00 +0700

    chore: update default token

--- apps/web/src/app/[lang]/(swap)/_utils/constants.ts
+++ apps/web/src/app/[lang]/(swap)/_utils/constants.ts
@@ -1,9 +1,9 @@
 export const DEFAULT_SELL_TOKEN =
   process.env.DEFAULT_SELL_TOKEN ||
-  "HXsKnhXPtGr2mq4uTpxbxyy7ZydYWJwx4zMuYPEDukY";
+  "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 export const DEFAULT_BUY_TOKEN =
   process.env.DEFAULT_BUY_TOKEN ||
-  "DdLxrGFs2sKYbbqVk76eVx9268ASUdTMAhrsqphqDuX";
+  "9BB6NFEcjBCtnNLFko2FqVQBq8HHM13kCyYcdQbgpump";
 export const MOCK_OWNER_ADDRESS =
   "CWEFmasA42838A3b5aW5GLg23gn5i1F1hD1YB1T1W92a";
 export const MOCK_SWAP_ID = "1";
