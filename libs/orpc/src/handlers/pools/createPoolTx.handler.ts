07cf3b9821f0f7186ee8c67a257c40cce4d702a1
jsonDoge <16711523+jsonDoge@users.noreply.github.com>
2025-08-09 13:34:08 +0300

    feat: create pool tx working

---
+++ libs/orpc/src/handlers/pools/createPoolTx.handler.ts
@@ -0,0 +1,135 @@
+import { BN, Program, web3 } from "@coral-xyz/anchor";
+import {
+  getAssociatedTokenAddressSync,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { PublicKey, type Transaction } from "@solana/web3.js";
+import IDL from "../../darklake-idl";
+import type {
+  CreatePoolTxInput,
+  CreatePoolTxOutput,
+} from "../../schemas/pools/createPoolTx.schema";
+
+// TODO: Move to constants file
+const POOL_SEED = "pool";
+const AMM_CONFIG_SEED = "amm_config";
+const LIQUIDITY_SEED = "lp";
+
+// devnet and mainnet uses different ones
+// should be re-generated from IDL
+const createPoolFeeVaultDevnet = new PublicKey(
+  "6vUjEKC5mkiDMdMhkxV8SYzPQAk39aPKbjGataVnkUss",
+);
+const createPoolFeeVaultMainnet = new PublicKey(
+  "HNQdnRgtnsgcx7E836nZ1JwrQstWBEJMnRVy8doY366A",
+);
+
+const createPoolFeeVault =
+  process.env.NETWORK === "2"
+    ? createPoolFeeVaultDevnet
+    : createPoolFeeVaultMainnet;
+
+async function createPool(
+  user: PublicKey,
+  program: Program<typeof IDL>,
+  tokenXMint: PublicKey,
+  tokenXProgramId: PublicKey,
+  tokenYMint: PublicKey,
+  tokenYProgramId: PublicKey,
+  depositAmountX: number,
+  depositAmountY: number,
+): Promise<Transaction> {
+  const [ammConfig] = PublicKey.findProgramAddressSync(
+    [Buffer.from(AMM_CONFIG_SEED), new BN(0).toArrayLike(Buffer, "le", 4)],
+    program.programId,
+  );
+
+  // Find pool
+  const [poolPubkey] = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(POOL_SEED),
+      ammConfig.toBuffer(),
+      tokenXMint.toBuffer(),
+      tokenYMint.toBuffer(),
+    ],
+    program.programId,
+  );
+
+  const [lpMint] = PublicKey.findProgramAddressSync(
+    [Buffer.from(LIQUIDITY_SEED), poolPubkey.toBuffer()],
+    program.programId,
+  );
+
+  const userTokenAccountLp = getAssociatedTokenAddressSync(
+    lpMint,
+    user,
+    false,
+    TOKEN_PROGRAM_ID,
+  );
+
+  // Create add liquidity transaction
+  const tx = await program.methods
+    .initializePool(new BN(depositAmountX), new BN(depositAmountY))
+    .accountsPartial({
+      createPoolFeeVault,
+      tokenMintX: tokenXMint,
+      tokenMintXProgram: tokenXProgramId,
+      tokenMintY: tokenYMint,
+      tokenMintYProgram: tokenYProgramId,
+      user,
+      userTokenAccountLp: userTokenAccountLp,
+    })
+    .transaction();
+
+  // Add compute budget instruction
+  const modifyComputeUnits = web3.ComputeBudgetProgram.setComputeUnitLimit({
+    units: 500_000,
+  });
+
+  tx.add(modifyComputeUnits);
+
+  return tx;
+}
+
+// Usage example same as addLiquidityTxHandler
+
+// Tries to create a pool with and deposit given amounts of tokenX and tokenY
+// (if pool exists or amounts too small it will fail)
+export async function createPoolTxHandler(
+  input: CreatePoolTxInput,
+): Promise<CreatePoolTxOutput> {
+  const {
+    user,
+    tokenXMint,
+    tokenYMint,
+    tokenXProgramId,
+    tokenYProgramId,
+    depositAmountX,
+    depositAmountY,
+    provider,
+  } = input;
+  const program = new Program(IDL, provider);
+
+  try {
+    const tx = await createPool(
+      new PublicKey(user),
+      program,
+      new PublicKey(tokenXMint),
+      new PublicKey(tokenXProgramId),
+      new PublicKey(tokenYMint),
+      new PublicKey(tokenYProgramId),
+      depositAmountX,
+      depositAmountY,
+    );
+    return {
+      success: true,
+      transaction: tx,
+    };
+  } catch (error) {
+    console.error("Error during liquidity addition:", error);
+    return {
+      success: false,
+      transaction: null,
+    };
+  }
+}
