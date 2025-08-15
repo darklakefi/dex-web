80c8037ede70c322c016e4b34d9c4eb8a3c91e49
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2025-08-15 14:45:31 +0200

    feat: initial posthog and sentry integration

---
+++ apps/web/sentry.client.config.ts
@@ -0,0 +1,33 @@
+import * as Sentry from "@sentry/nextjs";
+import posthog from "posthog-js";
+
+Sentry.init({
+  dsn: "https://773fd9bc5b4d5d1ff4d35335fce01b3c@o4508075926618112.ingest.de.sentry.io/4508075930484816",
+  // Replay may only be enabled for the client-side
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    posthog.sentryIntegration({
+      organization: "darklake",
+      projectId: 4508075930484816,
+      severityAllowList: ["error", "info"], // optional: here is set to handle captureMessage (info) and captureException (error)
+    }),
+  ],
+  replaysOnErrorSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // ...
+
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
---
+++ apps/web/src/instrumentation.ts
@@ -0,0 +1,17 @@
+import * as Sentry from "@sentry/nextjs";
+
+export async function register() {
+  if (
+    (process.env.NEXT_RUNTIME === "nodejs" ||
+      process.env.NEXT_RUNTIME === "edge") &&
+    process.env.SENTRY_DSN
+  ) {
+    Sentry.init({
+      debug: false,
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+      tracesSampleRate: 1,
+    });
+  }
+}
+
+export const onRequestError = Sentry.captureRequestError;
