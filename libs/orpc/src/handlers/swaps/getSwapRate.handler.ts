f622acbcd050a281d7dca52fb872a83c9bd196d9
Felix <hdcompany123@gmail.com>
2025-08-17 13:37:54 +0700

    chore: fix number format

--- libs/orpc/src/handlers/swaps/getSwapRate.handler.ts
+++ libs/orpc/src/handlers/swaps/getSwapRate.handler.ts
@@ -169,9 +169,8 @@ export async function getSwapRateHandler(
     );
     const roundedInput = scaledInput.integerValue(BigNumber.ROUND_DOWN);
 
-    const tradeFeeRate = BigNumber(ammConfig.trade_fee_rate) || BigNumber(0);
-    const protocolFeeRate =
-      BigNumber(ammConfig.protocol_fee_rate) || BigNumber(0);
+    const tradeFeeRate = BigNumber(ammConfig.trade_fee_rate || 0);
+    const protocolFeeRate = BigNumber(ammConfig.protocol_fee_rate || 0);
     const swapResult = calculateSwap(
       roundedInput,
       isXtoY ? availableReserveX : availableReserveY,
@@ -227,7 +226,7 @@ export async function getSwapRateHandler(
       .minus(newRate)
       .dividedBy(originalRate)
       .multipliedBy(100);
-    const priceImpactTruncated = priceImpact.integerValue(BigNumber.ROUND_DOWN);
+    const priceImpactTruncated = priceImpact.toFixed(2);
 
     return {
       amountIn,
@@ -235,7 +234,7 @@ export async function getSwapRateHandler(
       amountOut: amountOut.toNumber(),
       amountOutRaw: amountOutBigDecimal.toString(),
       estimatedFee: swapResult.tradeFee.toString(),
-      priceImpact: priceImpactTruncated.toNumber(),
+      priceImpact: Number(priceImpactTruncated),
       rate: adjustedRate.toNumber(),
       tokenX,
       tokenY,
