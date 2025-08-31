74bd39a23be442d15f6dfefb89adc4e6fcc88ab8
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-08-31 20:57:22 +0530

    feat(router): integrate create, delete, list, and update handlers for pinned pools

--- libs/orpc/src/routers/pools.router.ts
+++ libs/orpc/src/routers/pools.router.ts
@@ -3,13 +3,21 @@ import { getLPRate } from "../procedures/pools/getLPRate.procedure";
 import { getPinedPool } from "../procedures/pools/getPinedPool.procedure";
 import { getPoolDetails } from "../procedures/pools/getPoolDetails.procedure";
 import { getPoolReserves } from "../procedures/pools/getPoolReserves.procedure";
+import { createPinnedPool } from "../procedures/pools/pinned/createPinnedPool.procedure";
+import { deletePinnedPool } from "../procedures/pools/pinned/deletePinnedPool.procedure";
+import { listPinnedPools } from "../procedures/pools/pinned/listPinnedPools.procedure";
+import { updatePinnedPool } from "../procedures/pools/pinned/updatePinnedPool.procedure";
 
 export const poolsRouter = {
+  createPinnedPool,
   createPoolTransaction,
+  deletePinnedPool,
   getLPRate,
   getPinedPool,
   getPoolDetails,
   getPoolReserves,
+  listPinnedPools,
+  updatePinnedPool,
 };
 
 export type PoolsRouter = typeof poolsRouter;
