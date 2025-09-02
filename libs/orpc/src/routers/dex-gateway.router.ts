412b1b03ca2fe4884c8fd8c89c235f08530a1580
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-09-02 14:06:04 +0530

    feat: integrate custom tokens endpoints into dex-gateway router
    - Add custom tokens routes to main router
    - Wire up all CRUD endpoints
    - Configure proper middleware and error handling
    - Enable custom tokens API access

--- libs/orpc/src/routers/dex-gateway.router.ts
+++ libs/orpc/src/routers/dex-gateway.router.ts
@@ -1,4 +1,9 @@
 import { checkTradeStatus } from "../procedures/dex-gateway/checkTradeStatus.procedure";
+import { createCustomToken } from "../procedures/dex-gateway/custom-tokens/createCustomToken.procedure";
+import { deleteCustomToken } from "../procedures/dex-gateway/custom-tokens/deleteCustomToken.procedure";
+import { editCustomToken } from "../procedures/dex-gateway/custom-tokens/editCustomToken.procedure";
+import { getCustomToken } from "../procedures/dex-gateway/custom-tokens/getCustomToken.procedure";
+import { getCustomTokens } from "../procedures/dex-gateway/custom-tokens/getCustomTokens.procedure";
 import { getSwap } from "../procedures/dex-gateway/getSwap.procedure";
 import { getTokenMetadataList } from "../procedures/dex-gateway/getTokenMetadataList.procedure";
 import { getTradesListByUser } from "../procedures/dex-gateway/getTradesListByUser.procedure";
@@ -6,6 +11,11 @@ import { submitSignedTransaction } from "../procedures/dex-gateway/submitSignedT
 
 export const dexGatewayRouter = {
   checkTradeStatus,
+  createCustomToken,
+  deleteCustomToken,
+  editCustomToken,
+  getCustomToken,
+  getCustomTokens,
   getSwap,
   getTokenMetadataList,
   getTradesListByUser,
