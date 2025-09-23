5e8dfab874549beee622957a73c1837e9d7edb79
Felix <hdcompany123@gmail.com>
2025-09-23 14:16:56 +0700

    feat: enhance modal overlay background

--- libs/ui/src/lib/Modal/Modal.tsx
+++ libs/ui/src/lib/Modal/Modal.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import { twMerge } from "tailwind-merge";
+
 interface ModalProps {
   children: React.ReactNode;
   onClose: () => void;
+  className?: string;
 }
 /**
  * The Modal component is a modal dialog that can be used to display content in a overlay.
  */
-export function Modal({ children, onClose }: ModalProps) {
+export function Modal({ children, onClose, className }: ModalProps) {
   return (
     <dialog
       aria-modal="true"
-      className="fixed inset-0 z-50 flex h-full w-full items-center justify-center bg-black/50"
+      className={twMerge("fixed inset-0 z-50 flex h-full w-full items-center justify-center bg-black/50", className)}
       onClick={(e) => {
         if (e.target === e.currentTarget) {
           onClose();
